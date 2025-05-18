0eb1162d30496805ac5345fc4b649fdfabcfd1af
Antônio de Pádua <antonyejoao@gmail.com>
2025-05-18 19:14:52 -0300

    adicionando estrutura inicial do crud com métodos e orientação a objetos implementada

---
+++ src/LogicaDeProgramacao/desafiosChatGPT/desafio1/Main.java
@@ -0,0 +1,80 @@
+package LogicaDeProgramacao.desafiosChatGPT.desafio1;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Produto produto = new Produto();
+        boolean verificacao = true;
+
+        System.out.println("Bem vindo ao estoque de produtos");
+
+        while(verificacao) {
+            System.out.println("O que você deseja fazer:\n1 - Adicionar produto\n2 - Listar produto\n3 - editar produto\n4 - remover produto\n 5 - sair do programa");
+            int escolha = scan.nextInt();
+            scan.nextLine();
+
+            switch(escolha) {
+                case 1:
+                    System.out.println("Adicionando produto...");
+                    System.out.println("Digite o id do produto:");
+                    Long id = scan.nextLong();
+                    scan.nextLine();
+
+                    System.out.println("Digite o nome do produto:");
+                    String nome = scan.nextLine();
+
+                    System.out.println("Digite o preço do produto:");
+                    double preco = scan.nextDouble();
+                    scan.nextLine();
+
+                    System.out.println("Digite a quantidade do produto em estoque:");
+                    int quantidade = scan.nextInt();
+                    scan.nextLine();
+
+                    produto.criarProduto(produto,id , nome, preco, quantidade);
+                break;
+                case 2:
+                    System.out.println("Listando produto...");
+                    produto.exibirListaProduto();
+                break;
+                case 3:
+                    System.out.println("Editando produto...");
+                    System.out.println("Digite o id do produto:");
+                    id = scan.nextLong();
+
+                    System.out.println("Digite o nome do produto:");
+                    nome = scan.nextLine();
+
+                    System.out.println("Digite o preço do produto:");
+                    preco = scan.nextDouble();
+                    scan.nextLine();
+
+                    System.out.println("Digite a quantidade do produto em estoque:");
+                    quantidade = scan.nextInt();
+                    scan.nextLine();
+
+                    produto.editarProduto(id, nome, preco, quantidade);
+                    break;
+                case 4:
+                    System.out.println("Removendo produto...");
+                    System.out.println("Digite o id do produto:");
+                    id = scan.nextLong();
+
+
+                    produto.removerLista(id);
+                    break;
+                case 5:
+                    System.out.println("Obrigado por utilizar o programa!");
+                    verificacao = false;
+                break;
+                default:
+                    System.out.println("\nOpção inválida\n");
+                break;
+            }
+        }
+    }
+}
---
+++ src/LogicaDeProgramacao/desafiosChatGPT/desafio1/Produto.java
@@ -0,0 +1,135 @@
+package LogicaDeProgramacao.desafiosChatGPT.desafio1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Produto {
+
+    private Long id;
+    private String nome;
+    private double preco;
+    private int quantidade;
+    private List<Produto> listaProdutos = new ArrayList<>();
+
+    public Produto() {
+    }
+
+    public Produto(Long id, String nome, double preco, int quantidade) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public List<Produto> getListaProdutos() {
+        return listaProdutos;
+    }
+
+    public void setListaProdutos(List<Produto> listaProdutos) {
+        this.listaProdutos = listaProdutos;
+    }
+
+    public String somarValorTotalEmEstoque(int quantidade, double preco) {
+        double valorTotalDoEstoque = quantidade * preco;
+
+        return "O valor total do produto:" + this.nome + " de acordo com o que se possui em estoque é de: " + valorTotalDoEstoque;
+    }
+
+    public List exibirListaProduto() {
+        if(listaProdutos.isEmpty()) {
+            System.out.println("A lista está vazia.");
+        }
+
+        else {
+            for (Produto produto : listaProdutos) {
+                System.out.println("ID:" + produto.getId());
+                System.out.println("Nome:" + produto.getNome());
+                System.out.println("Preço:" + produto.getPreco());
+                System.out.println("Quantidade em estoque:" + produto.getQuantidade());
+
+            }
+        }
+
+        return listaProdutos;
+    }
+
+    public void criarProduto(Produto produto, Long id, String nome, double preco, int quantidade) {
+        produto.setId(id);
+        produto.setNome(nome);
+        produto.setPreco(preco);
+        produto.setQuantidade(quantidade);
+
+        this.nome = produto.getNome();
+        this.preco = produto.getPreco();
+        this.quantidade = produto.getQuantidade();
+
+
+        listaProdutos.add(produto);
+        System.out.println("Produto " + produto + " adicionado com sucesso!");
+    }
+
+    public void editarProduto(Long id, String nome, double preco, int quantidade) {
+        for(Produto p : listaProdutos) {
+            if(p.getId() == id) {
+                listaProdutos.stream().map( produtoEditado -> {
+                    p.setNome(nome);
+                    p.setPreco(preco);
+                    p.setQuantidade(quantidade);
+
+                    produtoEditado.setNome(p.getNome());
+                    produtoEditado.setPreco(p.getPreco());
+                    produtoEditado.setQuantidade(p.getQuantidade());
+
+                    return "Produto editado com sucesso:" + produtoEditado;
+                });
+            }
+        }
+    }
+
+    public void removerLista(Long id) {
+        for (Produto p : listaProdutos) {
+            if(p.getId().equals(id)) {
+                listaProdutos.remove(p);
+            } else {
+                System.out.println("O produto não existe.");
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Produto:\n" + "Nome do Produto: " + this.nome + "\nPreço do Produto: " + this.preco + "\nQuantidade do Produto: " + this.quantidade;
+    }
+}
